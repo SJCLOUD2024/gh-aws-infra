f36caa7b7c69da39c4708bf55959785581726a05
SJCLOUD2024 <sjnyame@outlook.com>
2025-06-02 12:28:59 +0100

    added s3

--- aws-deploy/VPC.tf
+++ aws-deploy/VPC.tf
@@ -126,3 +126,11 @@ resource "aws_route_table_association" "public-subnet-2-route-table-association"
   subnet_id           = aws_subnet.public-subnet-2.id
   route_table_id      = aws_route_table.public-route-table.id
 }
+
+terraform {
+  backend "s3" {
+    bucket = "github-state-sjcloud333 "
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
